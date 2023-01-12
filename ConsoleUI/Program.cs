4cb02f727edf1ac08a7193ef48bf2163e8c73f63
AhmettK <ahmetkayaalp42@hotmail.com>
2023-01-12 22:27:36 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,36 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICarService carManager = new CarManager(new InMemoryCarDal());
+
+            //ADD & DELETE
+            Car car1 = new Car { Id = 6, BrandId = 3, ColorId = 3, ModelYear = 2010, DailyPrice = 500, Description = "150 Beygir" };
+            carManager.Add(car1);
+            carManager.Delete(car1);
+
+            //UPDATE
+            Car car2 = new Car { Id = 3, BrandId = 3, ColorId = 3, ModelYear = 1997, DailyPrice = 275, Description = "105 Beygir" };
+            carManager.Update(car2);
+
+            //getAll() operasyonu
+            var cars = carManager.GetAll();
+            foreach (var item in cars)
+            {
+                Console.WriteLine(item.Description);
+            }
+
+            //getById() operasyonu
+            var car3 = carManager.GetById(5);
+            Console.WriteLine("\ngetById: " + car3.Description);
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemoryCarDal.cs
@@ -0,0 +1,54 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> {
+            new Car { Id=1,BrandId=1,ColorId=1,ModelYear=1994,DailyPrice=200,Description="100 Beygir"},
+            new Car { Id=2,BrandId=2,ColorId=2,ModelYear=1996,DailyPrice=250,Description="100 Beygir"},
+            new Car { Id=3,BrandId=3,ColorId=3,ModelYear=1997,DailyPrice=275,Description="100 Beygir"},
+            new Car { Id=4,BrandId=1,ColorId=1,ModelYear=2006,DailyPrice=350,Description="110 Beygir"},
+            new Car { Id=5,BrandId=2,ColorId=2,ModelYear=2007,DailyPrice=375,Description="125 Beygir"}
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            var carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int id)
+        {
+            return _cars.SingleOrDefault(c => c.Id == id);
+        }
+
+        public void Update(Car car)
+        {
+            var carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
